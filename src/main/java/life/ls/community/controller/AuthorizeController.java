b41bbdcfaf16df395aa4ea342db785412b239a08
Angry-stone3 <1145119454@qq.com>
2019-12-31 22:45:11 +0800

    add edit and update sign in

--- src/main/java/life/ls/community/controller/AuthorizeController.java
+++ src/main/java/life/ls/community/controller/AuthorizeController.java
@@ -1,11 +1,11 @@
 package life.ls.community.controller;
-import	java.util.Date;
+
 
 import life.ls.community.dto.AccessTokenDTO;
 import life.ls.community.dto.GitHubUser;
-import life.ls.community.mapper.UserMapper;
 import life.ls.community.model.User;
 import life.ls.community.provider.GithubProvider;
+import life.ls.community.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,7 +23,7 @@ import java.util.UUID;
 @Controller
 public class AuthorizeController {
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
     @Autowired
     private GithubProvider githubProvider;
@@ -56,24 +56,33 @@ public class AuthorizeController {
         GitHubUser gitHubUser = githubProvider.getUser(accessToken);
 
         //3.登录信息存储到数据库
-        if (gitHubUser!=null&&gitHubUser.getId()!=null) {
+        if (gitHubUser != null && gitHubUser.getId() != null) {
             //封装数据
-            User user=new User();
-            String token=UUID.randomUUID().toString();
+            User user = new User();
+            //登录的状态码
+            String token = UUID.randomUUID().toString();
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             user.setName(gitHubUser.getName());
-            user.setGmtCreate(new Date());
             user.setToken(token);
-            user.setGmtModified(new Date());
             user.setAvatarUrl(gitHubUser.getAvatarUrl());
-
-            userMapper.save(user);
+            //更新或创建用户
+            userService.createOrUpdate(user);
             //保存token到cookie
-            response.addCookie(new Cookie("token",token));
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
-        }else {
+        } else {
             return "redirect:/error";
         }
+    }
 
+    //退出登录
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,HttpServletResponse response){
+        //清空cookie以及去除session中user
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
--- src/main/java/life/ls/community/mapper/UserMapper.java
+++ src/main/java/life/ls/community/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import life.ls.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * 持久层：用户的mapper接口
@@ -31,4 +32,28 @@ public interface UserMapper {
      */
     @Select("select * from user where id=#{id}")
     User findById(Long id);
+
+    /**
+     * 通过accountId查询用户
+     * @param accountId
+     * @return
+     */
+    @Select("select * from user where account_id=#{accountId}")
+    User findByAccountId(String accountId);
+
+    /**
+     * 插入数据
+     * @param user
+     */
+    @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified,avatar_url)" +
+            "values(#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl}) ")
+    void insert(User user);
+
+    /**
+     * 更新用户
+     * @param dbUser
+     */
+    @Update("update user set name =#{name},token=#{token},gmt_modified=#{gmtModified},avatar_url=#{avatarUrl}" +
+            " where id=#{id}")
+    void update(User dbUser);
 }
