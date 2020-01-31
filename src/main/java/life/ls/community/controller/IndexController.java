2bf74cc34c055c9053bcde9b960f2bb70e4e2dbb
Angry-stone3 <1145119454@qq.com>
2020-01-31 15:52:44 +0800

    add search and so on

--- src/main/java/life/ls/community/controller/IndexController.java
+++ src/main/java/life/ls/community/controller/IndexController.java
@@ -4,8 +4,10 @@ import com.github.pagehelper.PageInfo;
 import life.ls.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -14,12 +16,13 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        @RequestParam(defaultValue = "1", value = "pageNum") Integer pageNum) {
-
+    public String index(Model model,
+                        @RequestParam(defaultValue = "1", value = "pageNum", required = false) Integer pageNum,
+                        @RequestParam(value = "search", required = false) String search) {
         //返回结果集到页面
-        PageInfo pageInfo = questionService.list(pageNum, 3);
-        request.setAttribute("pageInfo", pageInfo);
+        PageInfo pageInfo = questionService.list(search, pageNum, 3);
+        model.addAttribute("pageInfo", pageInfo);
+        model.addAttribute("search", search);
         return "index";
     }
 }
